94a34b5db4edf656b4830e0c30a4cf689e968b70
R-AsInRohit <rasinrohit@gmail.com>
2022-08-19 15:24:16 +0530

    firebase check

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:notes/utility/color_pallet.dart';
 import 'components/components.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+var db = FirebaseFirestore.instance;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
+  //testingDb();
+}
+
+void testingDb() {
+  final user = <String, dynamic>{
+    'first': '200',
+    'last': 418,
+  };
+  db.collection('users').add(user).then((value) => print(value));
 }
 
 class MyApp extends StatelessWidget {
@@ -14,12 +33,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: const MyAppBar(),
-        body: Container(
-          color: lightblack,
-        ),
-        bottomNavigationBar: const MyBottomNavBar()
-      ),
+          appBar: const MyAppBar(),
+          body: Container(
+            color: lightblack,
+          ),
+          bottomNavigationBar: const MyBottomNavBar()),
     );
   }
 }
