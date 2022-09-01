cca07f41991cfa969db1d72b2afc286ec6d5168e
R-AsInRohit <rasinrohit@gmail.com>
2022-09-01 16:59:58 +0530

    nre font

--- lib/utility/color_pallet.dart
+++ lib/utility/color_pallet.dart
@@ -5,7 +5,8 @@ const Color lightblack = Color(0xff191919);
 
 List<ThemeData> availableThemes = <ThemeData>[
    ThemeData(
-        colorScheme: const ColorScheme.light(
+    fontFamily: 'ubuntu',
+        colorScheme: const ColorScheme.light(        
             primary: Colors.white,
             secondary: Colors.red,
             onPrimary: Colors.black,
@@ -13,6 +14,7 @@ List<ThemeData> availableThemes = <ThemeData>[
             onBackground: Colors.black,
             onSecondary: Colors.white)),
     ThemeData(
+      fontFamily: 'ubuntu',
         colorScheme: const ColorScheme.light(
             primary: Colors.white,
             secondary: Colors.black,
@@ -21,6 +23,7 @@ List<ThemeData> availableThemes = <ThemeData>[
             onBackground: Colors.white,
             onSecondary: Colors.white)),
     ThemeData(
+      fontFamily: 'ubuntu',
         colorScheme: const ColorScheme.light(
             primary: Colors.white,
             secondary: Colors.blue,
